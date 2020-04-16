3b45a3da0092baadb4d63316ab0e0e364c36d909
Eduardo Mathías <eduardo.mathias@pm.me>
2020-04-16 16:25:11 -0300

    Add Entypo fonts. Add social links to nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,12 +3,28 @@ import Link from 'next/link'
 
 const Nav = () => (
   <nav className="navigation">
-    <Link href="/">
-      <span className="logotype">
-        <span>lex</span>
-        <span>lib</span>
-      </span>
-    </Link>
+    <div className="logotype-container">
+      <Link href="/">
+        <span className="logotype">
+          <span>lex</span>
+          <span>lib</span>
+        </span>
+      </Link>
+    </div>
+    <div className="social-container">
+      <ul>
+        <li>
+          <a href="https://github.com/eddmat/lexlib" title="Repositorio en Github" target="_blank">
+            <i className="icon-github"></i> <span>eddmat/lexlib</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/upthecomputers" title="Perfil en Twitter" target="_blank">
+            <i className="icon-twitter"></i> <span>@upthecomputers</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </nav>
 )
 
